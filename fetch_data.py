fa40ee5e14fb608ce770e26052098adf8ee2d835
Rostyslav <r.dobryanskyy@gmail.com>
2015-12-11 14:38:31 +0200

    Try to create list of all the usual words

--- fetch_data.py
+++ fetch_data.py
@@ -1,49 +1,30 @@
 __author__ = 'rostyslav'
 #this class should get data form the source
 
-import urllib2
-from HTMLParser import HTMLParser
-import re
+
 from bs4 import BeautifulSoup
 import nltk
-#nltk.download('all')
+
 
 response = open('The Project Gutenberg eBook of Planet of the Damned, by Harry Harrison.txt', 'r')
 html = response.read()
 
 
 
-def usual_words(text):
+def usual_words(unformated_text):
+    soup = BeautifulSoup(unformated_text,'lxml')
+    text_no_html = soup.get_text()
+    text = text_no_html.split(' ')
     usual = []
     text_vocab = set(w.lower() for w in text if w.isalpha())
     english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+    print text_vocab
     for w in text_vocab:
-        if w == english_vocab:
+        if w in english_vocab:
             usual.append(w)
-    return sorted(usual)
-
-print usual_words(html)
+    return usual
 
 
 
-"""
-soup = BeautifulSoup(html,'lxml')
 
-text_no_html = soup.get_text()
-
-#print "/*" + "<![" + ".+?>*/"
-#text = re.sub(r"/*<![CDATA[  XML blockout */ .+? /* XML end  ]]>*/", '', soup)
-
-style = soup.style
-
-text_list = text_no_html.split(' ')
-
-p_string = soup.p
-
-#print len(p_string)
-#for i in soup:
-#    print i
-
-#print text_list
-
-"""
+usual_words(html)
