3f151289ec25dd9cd6e1186d7e0d9fcf950eaef7
Rostyslav <r.dobryanskyy@gmail.com>
2015-12-12 20:23:16 +0200

    split parser and runner

--- fetch_data.py
+++ fetch_data.py
@@ -7,37 +7,36 @@ import nltk
 import markovlib
 import comunication_layer
 
-getfilefrom = comunication_layer.GetFile()
-html = getfilefrom.readfile()
 
-def usual_words(unformated_text):
-    soup = BeautifulSoup(unformated_text,'lxml')
-    text_no_html = soup.get_text()
-    text = text_no_html.split()
-    usual = []
-    text_vocab = set(w.lower() for w in text if w.isalpha())
-    english_vocab = set(w.lower() for w in nltk.corpus.words.words())
-    for w in text_vocab:
-        if w in english_vocab:
-            usual.append(w)
-    return usual
+class Create_Text(object):
 
-book_voc = usual_words(html)
-source_no_tags = (BeautifulSoup(html,'lxml')).get_text()
-
-string_source = str(source_no_tags)
-string_source = string_source.replace('\r\n',' ')
-string_source = string_source.replace('=97','')
-string_source = string_source.replace('attr(title)','')
-string_source = string_source.replace('=','')
-string_source = string_source.replace('[','')
-string_source = string_source.replace(']','')
-string_list = string_source.split()
+    def __init__(self,data):
+        self.data = unformated_text
 
 
-markov = markovlib.Markov(string_list)
+    def usual_words(self,unformated_text):
+        soup = BeautifulSoup(unformated_text,'lxml')
+        text_no_html = soup.get_text()
+        text = text_no_html.split()
+        usual = []
+        text_vocab = set(w.lower() for w in text if w.isalpha())
+        english_vocab = set(w.lower() for w in nltk.corpus.words.words())
+        for w in text_vocab:
+            if w in english_vocab:
+                usual.append(w)
+        return usual
 
-#print markov.generate_markov_text(500)
+    def generate_text(self,number):
+        book_voc = usual_words(html)
+        source_no_tags = (BeautifulSoup(html,'lxml')).get_text()
+        string_source = str(source_no_tags)
+        string_source = string_source.replace('\r\n',' ')
+        string_source = string_source.replace('=97','')
+        string_source = string_source.replace('attr(title)','')
+        string_source = string_source.replace('=','')
+        string_source = string_source.replace('[','')
+        string_source = string_source.replace(']','')
+        string_list = string_source.split()
+        markov = markovlib.Markov(string_list)
 
-
-print markov.generate_markov_text(1700)
+        print markov.generate_markov_text(number)
--- run.py
+++ run.py
@@ -1,1 +1,11 @@
 __author__ = 'rostyslav'
+import fetch_data
+import comunication_layer
+
+
+getfilefrom = comunication_layer.GetFile()
+html = getfilefrom.readfile()
+
+newbook = fetch_data.Create_Text(html)
+
+print newbook.generate_text(500)
