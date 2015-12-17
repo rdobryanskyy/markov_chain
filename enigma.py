02208ebca99d533e1d8ee50f0688b15629c5e807
Rostyslav <r.dobryanskyy@gmail.com>
2015-12-17 18:39:05 +0200

    Adding Decryption

--- enigma.py
+++ enigma.py
@@ -23,22 +23,6 @@ class Crypto(object):
     gears[72] = 'AZXWVYUSRQTPNMLOKIHGJFDCBE'
     gears[73] = 'AXYZWTUVSPQROLMNKHIJGDEFBC'
 
-    """
-    parser = argparse.ArgumentParser(description='Encrypt or decrypt enigma message.',
-                                     usage='%(prog)s [options]')
-    parser.add_argument('-k', '--key', type=str, help='Encryption key.')
-    parser.add_argument('-g', '--gears', type=int, nargs=3, choices=gears.keys(),
-                        help='Gears configuration.')
-    parser.add_argument('-e', '--encrypt', action='store_true',
-                        help='Encrypt message.')
-    parser.add_argument('-d', '--decrypt', action='store_true',
-                        help='Decrypt message.')
-    parser.add_argument('message', nargs=argparse.REMAINDER,
-                        help='Text to encrypt or decrypt.')
-
-    args = parser.parse_args()
-    """
-
 
     def encrypt(self, letter, params, first_gear):
 
@@ -57,26 +41,26 @@ class Crypto(object):
         return result
 
 
-    # def decrypt(self, letter, params, first_gear):
-    #     gear1 = params[0]
-    #     gear1letters = gear1['letters']
-    #     gear2 = params[1]
-    #     gear2letters = gear2['letters']
-    #     gear3 = params[2]
-    #     gear3letters = gear3['letters']
-    #
-    #     if first_gear:
-    #         pos_diff = gear1['keyIndex'] - gear1letters.index(letter)
-    #         pos_diff3 = gear3['keyIndex'] - pos_diff
-    #     else:
-    #         pos_diff = gear2['keyIndex'] - gear2letters.index(letter)
-    #         pos_diff3 = gear3['keyIndex'] + pos_diff
-    #
-    #     if pos_diff3 >= len(gear3letters):
-    #         pos_diff3 = pos_diff3 - len(gear3letters)
-    #     if pos_diff3 < -len(gear3letters):
-    #         pos_diff3 = pos_diff3 + len(gear3letters)
-    #     return gear3letters[pos_diff3]
+    def decrypt(self, letter, params, first_gear):
+        gear1 = params[0]
+        gear1letters = gear1['letters']
+        gear2 = params[1]
+        gear2letters = gear2['letters']
+        gear3 = params[2]
+        gear3letters = gear3['letters']
+
+        if first_gear:
+            pos_diff = gear1['keyIndex'] - gear1letters.index(letter)
+            pos_diff3 = gear3['keyIndex'] - pos_diff
+        else:
+            pos_diff = gear2['keyIndex'] - gear2letters.index(letter)
+            pos_diff3 = gear3['keyIndex'] + pos_diff
+
+        if pos_diff3 >= len(gear3letters):
+            pos_diff3 = pos_diff3 - len(gear3letters)
+        if pos_diff3 < -len(gear3letters):
+            pos_diff3 = pos_diff3 + len(gear3letters)
+        return gear3letters[pos_diff3]
 
 
     def init_gear(self, num, key):
@@ -114,28 +98,29 @@ class Crypto(object):
         return result
 
 
-    # def decrypt_message(self, gears, message):
-    #     count = 0
-    #     result = ''
-    #     first_gear = True
-    #     for m in message:
-    #         if m.upper() in alphabet:
-    #             result += decrypt(m.upper(), gears, first_gear)
-    #             first_gear = not first_gear
-    #             count += 1
-    #             if count == 4:
-    #                 result += ' '
-    #                 count = 0
-    #     return result
+    def decrypt_message(self, gears, message):
+        count = 0
+        result = ''
+        first_gear = True
+        for m in message:
+            if m.upper() in self.alphabet:
+                result += self.decrypt(m.upper(), gears, first_gear)
+                first_gear = not first_gear
+                count += 1
+                if count == 4:
+                    result += ' '
+                    count = 0
+        return result
 
 
-    def run_enigma(self, gear_one, gear_two, gear_three, enc_key, message):
+    def run_enigma_encryption(self, gear_one, gear_two, gear_three, enc_key, message):
 
         igears = self.init_gears(gear_one, gear_two, gear_three, enc_key)  # initiated gears
-        #
-        # if args.encrypt:
-        #     print encrypt_message(igears, ' '.join(args.message))
-        # elif args.decrypt:
         return self.encrypt_message(igears, ' '.join(message))
+
+    def run_enigma_decryption(self, gear_one, gear_two, gear_three, enc_key, message):
+
+        igears = self.init_gears(gear_one, gear_two, gear_three, enc_key)  # initiated gears
+        return self.decrypt_message(igears, ' '.join(message))
 
 
--- run.py
+++ run.py
@@ -7,9 +7,19 @@ import enigma
 getfilefrom = comunication_layer.GetFile()
 html = getfilefrom.readfile()
 newbook = fetch_data.Create_Text(html)
-
-book_for_enc = str(newbook.generate_text(5000))
-
+#Encrypting the book
 encrypt = enigma.Crypto()
+book_for_enc = str(newbook.generate_text(100))
+enc_book = encrypt.run_enigma_encryption(51,61,71,'AAA',book_for_enc.strip())
+
+
 print book_for_enc
-print encrypt.run_enigma(51,61,71,'AAA',book_for_enc.strip())
+print '\n' + enc_book
+
+#Decrypting the book
+
+print '-----------------------------------------------'
+
+decr_book = encrypt.run_enigma_decryption(51,61,71,'AAA',enc_book)
+
+print decr_book
